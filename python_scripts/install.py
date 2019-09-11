d6e53be99e5a984be16819d13159fdfec9151b43
Wersch <wersc004@bioinformatics.nl>
2019-09-11 11:10:35 +0200

    made some changes because vep installs in the directory you are

--- python_scripts/install.py
+++ python_scripts/install.py
@@ -19,16 +19,15 @@ def install(location):
                     "Ontologizer.jar",\
                     "ensembl-vep/vep",\
                     "nextflow"]
-    system("mkdir {}/plannotator".format(location))
-    system("git clone https://github.com/Ensembl/ensembl-vep.git {}/plannotator/ensembl-vep".format(location))
-    system("git checkout-release/96 {}/plannotator/ensembl-vep".format(location))
-    system("perl {}/plannotator/ensembl-vep/INSTALL.pl".format(location))
-    system("mkdir {}/plannotator/ensembl-vep/caches".format(location))
-    system("wget http://ontologizer.de/cmdline/Ontologizer.jar -P {}/plannotator".format(location))
-    system("cd {}/plannotator && curl -s https://get.nextflow.io | bash".format(location))
-    system("mkdir -p {}/plannotator/bin".format(location))
+    #system("git clone https://github.com/Ensembl/ensembl-vep.git {}".format(location))
+    #system("git checkout release/96 {}".format(location))
+    #system("perl {}/INSTALL.pl".format(location))
+    #system("mkdir {}/caches".format(location))
+    system("wget http://ontologizer.de/cmdline/Ontologizer.jar -P ../{}".format(location))
+    system("cd ../{} && curl -s https://get.nextflow.io | bash".format(location))
+    system("mkdir -p ../{}/bin".format(location))
     for file_loc in program_file_list:
-        system("ln -sf {}/plannotator/{} {}/plannotator/bin".format(location,file_loc, location))
+        system("ln -sf ../{}/{} ../{}/bin".format(location,file_loc, location))
 
 if __name__ == "__main__":
     loc = argv[1]
